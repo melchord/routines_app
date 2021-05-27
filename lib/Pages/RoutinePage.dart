0baa695038798bb8bcc877ed7ed9c0a55c1f3f0f
Melchor <mdmngz411@gmail.com>
2021-05-27 11:14:27 -0400

    - initial commit

---
+++ lib/Pages/RoutinePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RoutinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routine Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:routines/Pages/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Routines',
+    home: HomePage(),
+  ));
+}
