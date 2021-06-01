e2ee5eb8f5f4ca54e2d50681a1fd94becd4e44be
Melchor <mdmngz411@gmail.com>
2021-05-31 20:31:07 -0400

    - remove homepage

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:routines/Pages/RoutinePage.dart';
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Routine Page"),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RoutinePage()),
-            );
-          },
-        ),
-      )
-    );
-  }
+class HomePage extends StatefulWidget {
+  
 }
